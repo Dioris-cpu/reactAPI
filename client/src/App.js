71aa27d775280ab63f19fa3f5915e2a1edc3e329
Dioris Diaz <diorid@gmail.com>
2020-11-02 02:34:33 -0500

    api data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import FetchApiUsers from './componets/FetchApiUsers'
+import FetchApiUsers from './componets/FetchApiUsers';
+import Navbar from './componets/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <FetchApiUsers/>
     
     </div>
--- client/src/componets/FetchApiUsers.js
+++ client/src/componets/FetchApiUsers.js
@@ -3,34 +3,32 @@ import React from "react";
 export default class FetchRandomUser extends React.Component {
   state = {
     loading: true,
-    name: null,
+    person: null,
   };
 
-  // fetch pokemon data
   async componentDidMount() {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((response) => response.json())
-      .then((allpokemon) => console.log(allpokemon));
-      
-      
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results[0], loading: false });
   }
-  
 
   render() {
     if (this.state.loading) {
       return <div>loading...</div>;
     }
 
-    if (!this.state.name) {
+    if (!this.state.person) {
       return <div>didn't get a person</div>;
     }
 
     return (
-      <div>
-        <div>{this.state.name.title}</div>
-        {/* <div>{this.state.person.name.first}</div>
+      <div className="container">
+        <img className="img" src={this.state.person.picture.large} />
+        <div>{this.state.person.name.title}</div>
+        <div>{this.state.person.name.first}</div>
         <div>{this.state.person.name.last}</div>
-        <img src={this.state.person.picture.large} /> */}
+        {/* <button onClick={}></button> */}
       </div>
     );
   }
